82e9f60d1d96f0a0309e0b114f2ff60908f97a78
featherwalk <vacantorange@gmail.com>
2021-05-30 21:04:40 -0500

    provided for multiple users

--- index.js
+++ index.js
@@ -9,11 +9,15 @@ bot.on("ready", () => {
   console.info(`Logged in as ${bot.user.tag}!`);
 });
 
+function getUsernames(users) {
+  return users.map((user) => user.username);
+}
+
 bot.on("message", (msg) => {
   if (msg.content.startsWith("<3headpats")) {
     if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You gave ${taggedUser.username} a headpat!`);
+      const taggedUsers = getUsernames(msg.mentions.users);
+      msg.channel.send(`You gave ${taggedUsers.join(", and ")} a headpat!`);
     } else {
       msg.reply("you headpat yourself...?");
     }
