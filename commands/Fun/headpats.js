378dd669eabff6ab93bdb92f5d53f058e56327b5
featherwalk <vacantorange@gmail.com>
2021-09-01 21:13:47 -0500

    small adjustments - added Axios

--- commands/Fun/headpats.js
+++ commands/Fun/headpats.js
@@ -7,6 +7,7 @@ module.exports = {
   description: "Gives tagged member a headpat",
   execute(msg, args) {
     if (msg.mentions.users.size) {
+      console.log(msg.author.id);
       const taggedUsers = getUsernames(msg.mentions.users);
       if (taggedUsers.includes(msg.author.username)) {
         msg.reply("You headpat yourself...?");
